6c2b3b12bbbccde9cf6f84d95525edae6cc9383a
z00407nz <michal.rajtek@siemens.com>
2024-09-17 08:20:29 +0200

    Dokoncene cvicenie 005-DataTypes

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int age = 14;
+string name = "Michal";
+char inicialky = 'M';
+bool isAdult = true;
+float accountBalance = 20.50f;
+
+Console.WriteLine(accountBalance);
+
