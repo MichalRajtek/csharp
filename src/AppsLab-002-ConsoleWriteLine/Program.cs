eb0eaac8371436c6ad7660906d6a022f87afbde2
Michal Rajtek <michal.rajtek@siemens.com>
2025-09-10 08:55:52 +0200

    Zmena vypisovania na Hello Michal

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+//Vypise na konzoli text
+Console.WriteLine("Hello, Michal 1  !");
+Console.WriteLine("Hello, Michal 2 !");   //Vypise na konzoli text
+//Console.WriteLine("Hello, Michal 3 !");
+Console.WriteLine("Hello, Michal!  4  ");
+
+
+
+/*
+ 
+ 
+ 
+ * 
+ * Console.WriteLine("Hello, Michal!  4  ");
+Console.WriteLine("Hello, Michal!  4  ");
+Console.WriteLine("Hello, Michal!  4  ");
+Console.WriteLine("Hello, Michal!  4  ");
+Console.WriteLine("Hello, Michal!  4  ");\\\
+
+
+
+
+*/
